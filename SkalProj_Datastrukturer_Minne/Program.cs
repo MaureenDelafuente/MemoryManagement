c9f39e4557418926b1ddda510296c2b0ea206b9e
Maureen <maureendelafuente.b@gmail.com>
2024-09-27 14:41:47 +0200

    ExamineList done

--- SkalProj_Datastrukturer_Minne/Program.cs
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -10,10 +10,10 @@ namespace SkalProj_Datastrukturer_Minne
         /// <param name="args"></param>
         static void Main()
         {
-
             while (true)
             {
-                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                Console.WriteLine(
+                    "Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
                     + "\n1. Examine a List"
                     + "\n2. Examine a Queue"
                     + "\n3. Examine a Stack"
@@ -29,6 +29,7 @@ namespace SkalProj_Datastrukturer_Minne
                     Console.Clear();
                     Console.WriteLine("Please enter some input!");
                 }
+
                 switch (input)
                 {
                     case '1':
@@ -72,12 +73,43 @@ namespace SkalProj_Datastrukturer_Minne
              * Below you can see some inspirational code to begin working.
             */
 
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
-
-            //switch(nav){...}
+            List<string> theList = new List<string>();
+            while (true)
+            {
+                Console.WriteLine(
+                    " +word = add 'word' to the list\n -word = remove 'word' from the list\n x = exit to main menu");
+                string input = Console.ReadLine(); //  +Pölsa   -Pölsa
+                char nav = input[0]; // + or -
+                string value = input.Substring(1); // Pölsa
+                switch (nav)
+                {
+                    case '+':
+                        theList.Add(value);
+                        Console.WriteLine($"Add: '{value}', Count: {theList.Count}, Capacity: {theList.Capacity}");
+                        break;
+                    case '-':
+                        theList.Remove(value);
+                        Console.WriteLine($"Remove: '{value}', Count: {theList.Count}, Capacity: {theList.Capacity}");
+                        break;
+                    case 'x':
+                        return;
+                    default:
+                        Console.WriteLine("Use commands starting with + or -");
+                        break;
+                }
+            }
+            /*
+            2. När ökar listans kapacitet? (Alltså den underliggande arrayens storlek)
+            Den ökar när jag lägger till en element som det inte finns plats till.
+            3. Med hur mycket ökar kapaciteten?
+            Den blir dubbelt så stor.
+            4. Varför ökar inte listans kapacitet i samma takt som element läggs till?
+            För att det skulle ta mer tid att öka den så många gånger.
+            5. Minskar kapaciteten när element tas bort ur listan?
+            Nej.
+            6. När är det då fördelaktigt att använda en egendefinierad array istället för en lista?
+            När jag vet hur många element som behöver lagras.
+             */
         }
 
         /// <summary>
@@ -111,9 +143,6 @@ namespace SkalProj_Datastrukturer_Minne
              * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
              * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
              */
-
         }
-
     }
 }
-
